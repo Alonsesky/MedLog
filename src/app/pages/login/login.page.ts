4966e4399f30ea9f5254191fc0b1eb80ebd09799
Alo.gomezc <alo.gomezc@gmail.com>
2026-03-31 16:16:33 -0300

    Correccion de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,7 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonButton, IonInput } from '@ionic/angular/standalone';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonButton,
+  IonInput,
+} from '@ionic/angular/standalone';
 import { AuthenticationService } from '../../services/firebase/authentication.service';
 
 @Component({
@@ -9,20 +22,30 @@ import { AuthenticationService } from '../../services/firebase/authentication.se
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonButton, IonItem, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule]
+  imports: [
+    IonButton,
+    IonItem,
+    IonInput,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class LoginPage implements OnInit {
-
-  private authenticationService: AuthenticationService = inject(AuthenticationService);
-  private formBuilder: FormBuilder = inject(FormBuilder);
+  private authenticationService = inject(AuthenticationService);
+  private formBuilder = inject(FormBuilder);
 
   public formLogin = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['',Validators.required,Validators.minLength(6)]
+    password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
   constructor() {
-    this.authenticationService.authState.subscribe((user: any) => {
+    this.authenticationService.authState.subscribe((user:any) => {
       if (user) {
         console.log('User is logged in:', user);
       } else {
@@ -31,16 +54,30 @@ export class LoginPage implements OnInit {
     });
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 
   async login() {
-   await this.authenticationService.login(this.formLogin.value.email!, this.formLogin.value.password!);
+    if (this.formLogin.invalid) {
+      this.formLogin.markAllAsTouched();
+      return;
+    }
 
+    try {
+      const email = this.formLogin.value.email ?? '';
+      const password = this.formLogin.value.password ?? '';
+
+      const userCredential = await this.authenticationService.login(email, password);
+      console.log('Login correcto:', userCredential.user);
+    } catch (error) {
+      console.error('Error en login:', error);
+    }
   }
 
-  logout(){
-    this.authenticationService.logout();
+  async logout() {
+    try {
+      await this.authenticationService.logout();
+    } catch (error) {
+      console.error('Error en logout:', error);
+    }
   }
 }
