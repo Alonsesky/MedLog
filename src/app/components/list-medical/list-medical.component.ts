c646174c40c794d6c1d2a03b0865d27ec6365676
Alo.gomezc <alo.gomezc@gmail.com>
2026-04-10 14:20:12 -0400

    Implementacion de editar en list-medical

--- src/app/components/list-medical/list-medical.component.ts
+++ src/app/components/list-medical/list-medical.component.ts
@@ -1,21 +1,63 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Auth, authState } from '@angular/fire/auth';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { firstValueFrom } from 'rxjs';
 import { EvolutionService } from 'src/app/services/firebase/evolution.service';
 import { Evolution } from 'src/app/shared/models/evolution.model';
 import { CommonModule } from '@angular/common';
-import { IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonCardSubtitle } from "@ionic/angular/standalone";
+import {
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonButton,
+  IonCardSubtitle,
+  IonInput,
+  IonItem,
+  IonTextarea,
+  IonDatetime
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-list-medical',
   templateUrl: './list-medical.component.html',
   styleUrls: ['./list-medical.component.scss'],
-  imports: [IonCardSubtitle, IonButton, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCol, IonRow, IonGrid, CommonModule]
+  imports: [
+    IonCardSubtitle,
+    IonButton,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+    IonCard,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonInput,
+    IonItem,
+    IonTextarea,
+    IonDatetime,
+    CommonModule,
+    ReactiveFormsModule
+  ]
 })
 export class ListMedicalComponent  implements OnInit {
 
+  private fb = inject(FormBuilder);
   private evolutionService = inject(EvolutionService);
   evolutions: Evolution[] = [];
+  editingId: string | null = null;
+  isSaving = false;
+
+  form = this.fb.group({
+    emailPatient: ['', [Validators.required, Validators.email]],
+    namePatient: ['', [Validators.required, Validators.minLength(3)]],
+    nameProfessional: ['', [Validators.required, Validators.minLength(3)]],
+    date: ['', Validators.required],
+    evolution: ['', [Validators.required, Validators.minLength(3)]],
+  });
 
   constructor(
     private auth: Auth
@@ -43,7 +85,39 @@ export class ListMedicalComponent  implements OnInit {
 
   }
 
-  update(id: string) {
+  update(item: Evolution) {
+    this.editingId = item.id ?? null;
+    this.form.reset({
+      emailPatient: item.emailPatient,
+      namePatient: item.namePatient,
+      nameProfessional: item.nameProfessional,
+      date: item.date,
+      evolution: item.evolution,
+    });
+  }
+
+  cancelEdit() {
+    this.editingId = null;
+    this.isSaving = false;
+    this.form.reset();
+  }
+
+  async confirmUpdate(id: string) {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.isSaving = true;
+
+    try {
+      await this.evolutionService.UpdateById(id, this.form.getRawValue() as Partial<Evolution>);
+      this.cancelEdit();
+      console.log('Actualizado correctamente');
+    } catch (err) {
+      this.isSaving = false;
+      console.error(err);
+    }
 
   }
 
